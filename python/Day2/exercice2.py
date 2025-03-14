e25fa7d21d874298b580b313601a8fe057fd9a46
souleyman-NSM <yacef_s@etna-alternance.net>
2025-03-14 04:03:29 +0100

    day2

---
+++ python/Day2/exercice2.py
@@ -0,0 +1,16 @@
+number1 = input("Entrez un premier nombre :") 
+number2 = input("Entrez un deuxieme nombre :")
+
+somme = int(number1) + int(number2)
+
+difference = int(number1) - int(number2)
+
+produit = int(number1) * int(number2)
+
+quotient = int(number1) / int(number2)
+
+
+print("Somme :", somme,
+      "difference :", difference,
+      "produit :", produit,
+      "quotient :", quotient)
