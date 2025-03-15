614533c336bba75ee8854023ccd32af4ca138d43
souleyman-NSM <yacef_s@etna-alternance.net>
2025-03-15 17:42:33 +0100

    while

---
+++ python/Day2/bool/exercice5.py
@@ -0,0 +1,14 @@
+first = int(input("Entrez un premier nombre :"))
+
+second = int(input("Entrez un deuxieme nombre :"))
+
+
+if first > second:
+    print(first," est plus grand que", second)
+
+elif first == second:
+    print(first,"est égal avec", second)
+
+else:
+    first < second
+    print(first, "est plus petit que", second)
