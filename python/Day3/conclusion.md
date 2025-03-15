729145e18ea0efccd8b9a36abfcf4fbcb74ee853
souleyman-NSM <yacef_s@etna-alternance.net>
2025-03-16 00:03:19 +0100

    exo1

---
+++ python/Day3/conclusion.md
@@ -0,0 +1,3 @@
+Boucle for
+
+La boucle for permet de parcourir une séquence (comme une liste) et d'exécuter un bloc de code pour chaque élémen
---
+++ python/Day3/exercice1.py
@@ -0,0 +1,13 @@
+fruits = ["pomme", "banane", "orange"]
+
+print("Liste initiale :", fruits)
+
+print("Premier fruit :", fruits[0])
+
+print("dernier fruit :", fruits[2])
+
+fruits[2] = "pamplemouse"
+
+fruits.remove("banane")
+
+print("Liste mise à jour :", fruits)
